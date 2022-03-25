7c0fa30248ab91a29e91cb863bd43cc469b3994f
Francis Pham <francis@Franciss-MacBook-Pro.local>
2022-03-25 13:47:41 -0700

    Finish report bug page with code logic aand html, fix sort bug by new or popular, adding new radio component for the report page, delete one category from the database and code

---
+++ BugTrackerUI/Models/ReportBugModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BugTrackerUI.Models;
+
+public class ReportBugModel
+{
+     [Required]
+     [MaxLength(75)]
+     public string Bug { get; set; }
+
+     [Required]
+     [MinLength(1)]
+     [Display(Name = "Category")]
+     public string CategoryId { get; set; }
+
+     [MaxLength(500)]
+     public string Description { get; set; }
+}
+
