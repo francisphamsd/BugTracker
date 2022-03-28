e8656a40eaa02dcebd632d31c0be64b911805e1d
Francis Pham <francis@Franciss-MacBook-Pro.local>
2022-03-28 14:23:11 -0700

    adding Azure B2C log in system. Added login Display page. Bug fixing to make the login page works

--- BugTrackerUI/RegisterServices.cs
+++ BugTrackerUI/RegisterServices.cs
@@ -1,12 +1,28 @@
-﻿namespace BugTrackerUI;
+﻿using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.Identity.Web;
+using Microsoft.Identity.Web.UI;
+
+namespace BugTrackerUI;
 
 public static class RegisterServices
 {
      public static void ConfigureServices(this WebApplicationBuilder builder)
      {
           builder.Services.AddRazorPages();
-          builder.Services.AddServerSideBlazor();
+          builder.Services.AddServerSideBlazor().AddMicrosoftIdentityConsentHandler();
           builder.Services.AddMemoryCache();
+          builder.Services.AddControllersWithViews().AddMicrosoftIdentityUI();
+
+          builder.Services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
+               .AddMicrosoftIdentityWebApp(builder.Configuration.GetSection("AzureAdB2C"));
+
+          builder.Services.AddAuthorization(options =>
+          {
+               options.AddPolicy("Admin", policy =>
+               {
+                    policy.RequireClaim("jobTitle", "Admin");
+               });
+          });
 
           builder.Services.AddSingleton<IDbConnection, DbConnection>();
           builder.Services.AddSingleton<ICategoryData, MongoCategoryData>();
@@ -15,4 +31,3 @@ public static class RegisterServices
           builder.Services.AddSingleton<IUserData, MongoUserData>();
       }
 }
- 
